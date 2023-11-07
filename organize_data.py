48ea92ef99289ab93120917956ecf4f3eed0b77f
Joseph Zhang <josephzhang999@gmail.com>
2023-11-07 10:03:22 -0500

    Wrote data cleaning

--- organize_data.py
+++ organize_data.py
@@ -1,13 +1,11 @@
 import pandas as pd
 import sys
 
-df = pd.read_parquet(sys.argv[1])
+df = pd.read_parquet(sys.argv[1] + 'output.parquet')
 print(len(df.index), 'rows originally')
 
-current_stocks = df.loc[df['LINKENDDT'] == 'E']
-print(current_stocks.head())
-print(len(current_stocks.index), 'rows after removing inactive stocks')
-# df.sort_values(['
+df['adjpc'] = df['prccd'] / df['ajexdi']
+df = df.drop(columns=['LIID', 'iid', 'adrrc']).sort_values(['datadate', 'tic']).reset_index(drop=True).dropna(subset=['adjpc'])
 
-
-
+print(df)
+df.to_parquet(sys.argv[1] + 'clean.parquet')
